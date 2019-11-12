4b640fe8064388a09b964a57859822ee24390de3
Elvar Örn Unnþórsson <ellioman@ellioman.com>
2019-11-12 14:00:02 +0100

    Changed the default settings for transparent objects receiving shadows to true

--- com.unity.render-pipelines.universal/Runtime/Data/UniversalRenderPipelineAsset.cs
+++ com.unity.render-pipelines.universal/Runtime/Data/UniversalRenderPipelineAsset.cs
@@ -146,7 +146,7 @@ namespace UnityEngine.Rendering.Universal
         [SerializeField] float m_ShadowDepthBias = 1.0f;
         [SerializeField] float m_ShadowNormalBias = 1.0f;
         [SerializeField] bool m_SoftShadowsSupported = false;
-        [SerializeField] bool m_ShadowTransparentReceiveSupported = false;
+        [SerializeField] bool m_ShadowTransparentReceiveSupported = true;
 
         // Advanced settings
         [SerializeField] bool m_UseSRPBatcher = true;
